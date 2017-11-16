05c331e29ae6c00299ad8f0b6a310231a3ec4e8d
Alexandre <a.ferlet@student.isartdigital.com>
2017-11-16 22:58:35 +0100

    first commit

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public float speed = 10;
+
+    [Range(0,40)]
+    public float jumpForce = 10;
+
+    public GameObject Camera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float strafe = Input.GetAxis("Horizontal");
+        float move = Input.GetAxis("Vertical");
+
+        Debug.DrawRay(transform.position, Vector3.forward, Color.red, Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x + strafe * Time.deltaTime * speed, transform.position.y, transform.position.z + move * Time.deltaTime * speed);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody>().AddForce(0, jumpForce, 0, ForceMode.VelocityChange);
+        }
+
+    }
+}
---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+
+    [SerializeField]
+    [Range(0, 20)]
+    protected float cameraSpeed = 10;
+
+    [SerializeField]
+    protected float lockUp = 85;
+    [SerializeField]
+    protected float lockDown = -85;
+
+    // 
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        float lookV = Input.GetAxis("Mouse Y");
+        float lookH = Input.GetAxis("Mouse X");
+
+        transform.Rotate(Vector3.up, lookH, Space.World);
+        transform.Rotate(Vector3.right, -lookV, Space.Self);
+
+        if (Mathf.DeltaAngle(transform.rotation.eulerAngles.x, 0) > lockUp)
+        {
+            transform.rotation = Quaternion.Euler(-lockUp, transform.rotation.eulerAngles.y, 0);
+        }
+        if (Mathf.DeltaAngle(transform.rotation.eulerAngles.x, 0) < lockDown)
+        {
+            transform.rotation = Quaternion.Euler(-lockDown, transform.rotation.eulerAngles.y, 0);
+        }
+
+        //Changer des positions
+        //transform.position = new Vector3(transform.position.x + speed * Time.deltaTime * lookH, transform.position.y + speed * Time.deltaTime * lookV, 0);
+    }
+    
+}
